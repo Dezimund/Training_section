0bde8a863f0c347ffd986b8ffee5e90e9b59d55e
Dezimund <de3mond15@gmail.com>
2025-05-27 22:06:21 +0300

    Done

--- Training_section/Course/Home2.py
+++ Training_section/Course/Home2.py
@@ -8,4 +8,9 @@ number3 = numbers // 100 % 10
 number4 = numbers // 1000 % 10
 number5 = numbers // 10000
 
-print(f"{number1}{number2}{number3}{number4}{number5}")
+print(number1,end="")
+print(number2,end="")
+print(number3,end="")
+print(number4,end="")
+print(number5)
+
