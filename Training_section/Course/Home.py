ac9b51f033aafb15aebdc6caf349c36aa194f76f
Dezimund <de3mond15@gmail.com>
2025-05-27 21:42:31 +0300

    Done

---
+++ Training_section/Course/Home.py
@@ -0,0 +1,18 @@
+print("input 4 numbers")
+user_input = input()
+numbers = int (user_input)
+
+number1 = numbers // 1000
+number2 = numbers // 100 % 10
+number3 = numbers // 10 % 10
+number4 = numbers % 10
+
+print(number1)
+print(number2)
+print(number3)
+print(number4)
+
+
+
+
+
