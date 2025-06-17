47efa016369ba649732b477489afbb3081a7f3a5
Dezimund <de3mond15@gmail.com>
2025-06-17 17:34:57 +0300

    Upgrade

--- Training_section/Course/Home9.py
+++ Training_section/Course/Home9.py
@@ -5,17 +5,18 @@ user_input = input("Enter a string: ")
 
 if not user_input:
     print(False)
+elif ' ' in user_input:
+    print(False)
 elif user_input[0].isdigit():
     print(False)
-elif '__' in user_input:
+elif any(c.isupper() for c in user_input):
     print(False)
-elif any(i.isupper() for i in user_input):
-    print(False)
-elif any(i in string.punctuation.replace('_', '') for i in user_input):
+elif any(c in string.punctuation.replace('_', '') for c in user_input):
     print(False)
 elif user_input in keyword.kwlist:
     print(False)
 else:
     print(True)
+
 
 
