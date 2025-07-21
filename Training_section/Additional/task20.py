e0ef97654dfa5c589cb039365cef08a131ccb3fe
Dezimund <de3mond15@gmail.com>
2025-07-22 02:01:34 +0300

    Add

---
+++ Training_section/Additional/task20.py
@@ -0,0 +1,60 @@
+# Напишіть програму, яка запитує у користувача суму у євро і потім виводить:
+# мінімальну кількисть купюр та мінімальну кількість монет для того, щоб
+# отримати цю суму у євро. Зверніть увагу на те, що банкноти євро мають
+# номінали: 5, 10, 20, 50, 100, 200 та 500 евро. А монети: 1 та 2 євро,
+# 1, 2, 5, 10, 20 та 50 євроцентів.
+# Крім того треба надрукувати кількість кожного номіналу котрий
+# використовується для отримання даної суми.
+# * Бажано, щоб суму можна було вводити як через крапку так і через кому.
+
+# Приклад: Введіть суму: 347,78
+# Кількість банкнот: 5
+# Кількість монет: 7
+# Склад суми:
+# 1 x 200
+# 1 x 100
+# 2 x 20
+# 1 x 5
+# 2 x 1
+# 1 x 0.5
+# 1 x 0.2
+# 1 x 0.05
+# 1 x 0.02
+# 1 x 0.01
+
+
+summa = float(input("Введіть суму: ").replace(',', '.'))
+summa_euros = int(summa)
+summa_cents = round((summa - summa_euros) * 100)
+
+banknotes = [500, 200, 100, 50, 20, 10, 5]
+coins = [1, 2, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01]
+
+number_of_banknotes = 0
+number_of_coins = 0
+result = ['Склад суми:',]
+
+remainder = summa_euros
+for item in banknotes:
+    number = remainder // item
+    remainder %= item
+    if number > 0:
+        number_of_banknotes += number
+        result.append(f'{number} x {item}')
+    if remainder == 0:
+        break
+
+remainder = (remainder * 100) + summa_cents
+for item in coins:
+    number = remainder // (int(item * 100))
+    remainder %= (int(item * 100))
+    if number > 0:
+        number_of_coins += number
+        result.append(f'{number} x {item}')
+    if remainder == 0:
+        break
+
+print('Кількість банкнот:', number_of_banknotes)
+print('Кількість монет:', number_of_coins)
+if len(result) > 1:
+    print('\n'.join(result))
---
+++ Training_section/Additional/task8.py
@@ -0,0 +1,24 @@
+# Напишіть програму, яка запитує користувача три числа і виводить їх у порядку
+# зростання, розділені комою. Використовуйте умовні оператори та вкладені
+# умови для вирішення завдання. Передбачається, що це три числа різні.
+
+# Приклад:
+# Введіть перше число: 5
+# Введіть друге число: 2
+# Введіть третє число: 7
+
+# Числа в порядку зростання: 2, 5, 7
+
+
+first_number = int(input('Введіть перше число: '))
+second_number = int(input('Введіть друге число: '))
+third_number = int(input('Введіть третє число: '))
+
+if first_number > second_number:
+    first_number, second_number = second_number, first_number
+if second_number > third_number:
+    second_number, third_number = third_number, second_number
+if first_number > second_number:
+    first_number, second_number = second_number, first_number
+
+print(f'Числа в порядку зростання: {first_number}, {second_number}, {third_number}')
